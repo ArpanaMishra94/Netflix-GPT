3b248dcb15c4c538b3d08740c37c98b5b95eb09a
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-12-05 10:11:01 +0530

    sign in / sign up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import { useState, useRef } from 'react';
 import Header from './Header';
 import { checkValidData } from '../utils/validate';
+import { auth } from '../utils/firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true);
@@ -13,8 +15,38 @@ const Login = () => {
         // Validate the form data
         const message = checkValidData(email.current.value, password.current.value);
         setErrorMessage(message);
+        if (message) return;
 
-        if(message) return;
+        if (!isSignInForm) {
+            createUserWithEmailAndPassword(
+                auth,
+                email.current.value,
+                password.current.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + "-" + errorMessage);
+                });
+        }
+        else {
+            signInWithEmailAndPassword(
+                auth,
+                email.current.value,
+                password.current.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + "-" + errorMessage);
+                });
+        }
     }
 
     const toggleSignInForm = () => {
