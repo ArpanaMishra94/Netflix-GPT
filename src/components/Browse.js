cb294ff9870b2658cbec521899c73a09c648969d
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-12-10 19:51:54 +0530

    movies slice and Register TMDB API

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,9 +1,15 @@
+import useNowPlayingMovies from '../hooks/usNowPlayingMovies'
 import Header from './Header'
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 
 const Browse = () => {
+    useNowPlayingMovies();
     return (
         <div>
             <Header />
+            <MainContainer/>
+            <SecondaryContainer/>
         </div>
     )
 }
