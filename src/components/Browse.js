aa20acbc39846f0bf186e549158d0930a7d24c80
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-12-12 22:31:01 +0530

    secondary container with movie list and usePopular movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,15 +1,18 @@
-import useNowPlayingMovies from '../hooks/usNowPlayingMovies'
-import Header from './Header'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
+import Header from './Header';
+import usePopularMovies from '../hooks/usePopularMovies';
 import MainContainer from './MainContainer';
 import SecondaryContainer from './SecondaryContainer';
 
 const Browse = () => {
     useNowPlayingMovies();
+    usePopularMovies();
+
     return (
         <div>
             <Header />
-            <MainContainer/>
-            <SecondaryContainer/>
+            <MainContainer />
+            <SecondaryContainer />
         </div>
     )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,11 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const USER_AVATAR = "https://avatars.githubusercontent.com/u/90963767?v=4"
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTlkYWJhY2ZmNjkzNjhlNzc5ODkxMDZlNTI4YzQ4ZiIsInN1YiI6IjY1NzU1NDViN2EzYzUyMDBlYmZkNTBmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mUi2RrPjACUSVHaAAEkoLBude6pCGdGvBwsqy1xQssc'
-    }
-  };
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTlkYWJhY2ZmNjkzNjhlNzc5ODkxMDZlNTI4YzQ4ZiIsInN1YiI6IjY1NzU1NDViN2EzYzUyMDBlYmZkNTBmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mUi2RrPjACUSVHaAAEkoLBude6pCGdGvBwsqy1xQssc'
+  }
+};
+
+export const IMG_CON_URL = "https://image.tmdb.org/t/p/w500";
